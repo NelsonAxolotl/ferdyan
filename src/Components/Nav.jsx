884749d872f7b4e8b1dc7404a8271207ea11050a
NELSON PARAISO <paraisonelson@gmail.com>
2024-10-15 15:05:37 +0200

    alt

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,11 +1,12 @@
 import "./Nav.css";
 import logo from "../Pics/logomanu.webp";
 import { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const Nav = () => {
   const [showLinks, setShowLinks] = useState(false);
   const location = useLocation();
+  const navigate = useNavigate();
   const [activeLink, setActiveLink] = useState(location.pathname);
 
   useEffect(() => {
@@ -16,33 +17,70 @@ const Nav = () => {
     setShowLinks(!showLinks);
   };
 
+  const handleNavClick = (path) => {
+    setShowLinks(false);
+    setActiveLink(path);
+    navigate(path);
+  };
+
   return (
     <nav
       className={`navbar ${showLinks ? "show-nav" : "hide-nav"}`}
       role="navigation"
+      aria-label="Main navigation"
     >
-      <div className="navbar-logo" aria-label="Retour à l'accueil">
+      <div
+        className="navbar-logo"
+        onClick={() => handleNavClick("/")}
+        aria-label="Retour à l'accueil"
+      >
         <Link to="/">
-          <img src={logo} alt="logo Ferdyan" />
+          <img
+            src={logo}
+            alt="logo Ferdyan"
+            width="auto"
+            height="120px"
+            loading="eager"
+          />
         </Link>
       </div>
       <ul className="navbar-links">
         {[
-          { path: "/", label: "Accueil" },
-          { path: "/creation-sur-mesure", label: "Creation sur mesure" },
-          { path: "/chantournage", label: "Chantournage" },
-          { path: "/dessins", label: "Dessins" },
-          { path: "/contact", label: "Contact" },
-        ].map(({ path, label }, index) => (
-          <li
-            key={index}
-            className={`navbar-item ${activeLink === path ? "active" : ""}`}
-          >
-            <Link className="navbar-link" to={path}>
-              {label}
-            </Link>
-          </li>
-        ))}
+          { path: "/", label: "Accueil", className: "slideInDown-1" },
+          {
+            path: "/creation-sur-mesure",
+            label: "Création sur mesure",
+            className: "slideInDown-2",
+          },
+          {
+            path: "/chantournage",
+            label: "Chantournage",
+            className: "slideInDown-3",
+          },
+          { path: "/dessins", label: "Dessins", className: "slideInDown-4" },
+          { path: "/contact", label: "Contact", className: "slideInDown-5" },
+        ].map(
+          (
+            { path, label, className },
+            index // ici, déstructuration de className ajoutée
+          ) => (
+            <li
+              key={index}
+              className={`navbar-item ${className} ${
+                activeLink === path ? "active" : ""
+              }`}
+            >
+              <Link
+                className="navbar-link"
+                to={path}
+                onClick={() => handleNavClick(path)}
+                aria-label={`Navigate to ${label}`}
+              >
+                {label}
+              </Link>
+            </li>
+          )
+        )}
       </ul>
       <button
         className="navbar-burger"

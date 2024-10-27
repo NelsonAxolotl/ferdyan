a2e12fe25e7e2a5ab233c71cd029f02a7428094f
NELSON PARAISO <paraisonelson@gmail.com>
2024-10-27 13:17:16 +0100

    zertyuiopqsdfghjk

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -19,13 +19,7 @@ const Home = () => {
           <div className="picture">
             <div className="image-container">
               <Link to="/creation-sur-mesure">
-                <img
-                  src={escalier}
-                  alt="escalier"
-                  width="600"
-                  height="400"
-                  loading="lazy"
-                />
+                <img src={escalier} alt="escalier" width="600" height="400" />
                 <div className="image-text">Création sur mesure</div>
               </Link>
             </div>

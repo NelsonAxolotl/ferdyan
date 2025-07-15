da8b05f37fa5986cb759367c79e9c652f31306de
NELSON PARAISO <paraisonelson@gmail.com>
2025-07-15 19:49:52 +0200

    before

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import { useState, useEffect, lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import ScrollToTop from "./Components/ScrollToTop";
 import Nav from "./Components/Nav";
 import End from "./Components/End";
 import Intro from "./Components/Intro";
-
-// Lazy loading des pages
-const Home = lazy(() => import("./Pages/Home"));
-const Accueil = lazy(() => import("./Pages/Accueil"));
-const Chantournage = lazy(() => import("./Pages/Chantournage"));
-const Dessin = lazy(() => import("./Pages/Dessin"));
-const Creation = lazy(() => import("./Pages/Creation"));
-const Fixe = lazy(() => import("./Pages/Fixe"));
-const Contact = lazy(() => import("./Pages/Contact"));
-const NotFound = lazy(() => import("./Pages/NotFound"));
+import Home from "./Pages/Home";
+import Accueil from "./Pages/Accueil";
+import Chantournage from "./Pages/Chantournage";
+import Dessin from "./Pages/Dessin";
+import Creation from "./Pages/Creation";
+import Fixe from "./Pages/Fixe";
+import Contact from "./Pages/Contact";
+import NotFound from "./Pages/NotFound";
 
 function App() {
-  const [showIntro, setShowIntro] = useState(true);
+  const [showIntro, setShowIntro] = useState(true); // État pour afficher l'introduction
 
   const handleIntroComplete = () => {
-    setShowIntro(false);
+    setShowIntro(false); // Masquer l'introduction une fois terminée
   };
 
   useEffect(() => {
+    // Vérifiez la route actuelle pour réinitialiser l'introduction si nécessaire
     const handleNavigation = () => {
       if (window.location.pathname !== "/") {
-        setShowIntro(false);
+        setShowIntro(false); // Ne pas montrer l'intro si on n'est pas sur la route d'accueil
       }
     };
 
@@ -34,7 +34,6 @@ function App() {
       window.removeEventListener("popstate", handleNavigation);
     };
   }, []);
-
   useEffect(() => {
     const disableContextMenu = (event) => {
       if (event.target.tagName === "IMG") {
@@ -49,26 +48,28 @@ function App() {
     };
   }, []);
 
-  return showIntro ? (
-    <Intro onComplete={handleIntroComplete} />
-  ) : (
-    <div className="app-content">
-      <Nav />
-
-      <Suspense>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/creation-sur-mesure" element={<Creation />} />
-          <Route path="/reparation" element={<Fixe />} />
-          <Route path="/chantournage" element={<Chantournage />} />
-          <Route path="/bio" element={<Accueil />} />
-          <Route path="/dessins" element={<Dessin />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Suspense>
-      <End />
-    </div>
+  return (
+    <Router>
+      {showIntro ? (
+        <Intro onComplete={handleIntroComplete} />
+      ) : (
+        <div className="app-content">
+          <ScrollToTop />
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/creation-sur-mesure" element={<Creation />} />
+            <Route path="/reparation" element={<Fixe />} />
+            <Route path="/chantournage" element={<Chantournage />} />
+            <Route path="/bio" element={<Accueil />} />
+            <Route path="/dessins" element={<Dessin />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <End />
+        </div>
+      )}
+    </Router>
   );
 }
 
